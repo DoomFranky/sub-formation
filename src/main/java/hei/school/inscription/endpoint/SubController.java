37a50c936dcc1463ede9b8710a22d430b450d66d
Randy <raoulrandy3@gmail.com>
2026-07-03 09:35:33 +0300

    update(controller): add service to controller

--- src/main/java/hei/school/inscription/endpoint/SubController.java
+++ src/main/java/hei/school/inscription/endpoint/SubController.java
@@ -1,29 +1,27 @@
 package hei.school.inscription.endpoint;
 
+import hei.school.inscription.dto.UserDto;
+import hei.school.inscription.endpoint.event.EventProducer;
+import hei.school.inscription.endpoint.event.model.SendEmailRequested;
 import hei.school.inscription.mail.Email;
 import hei.school.inscription.mail.Mailer;
 import jakarta.mail.internet.InternetAddress;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class SubController {
-    private final Mailer mailer;
+    private final EventProducer<SendEmailRequested> eventProducer;
 
-    @GetMapping("/sub")
+    @PostMapping("/sub")
     @SneakyThrows
-    public String helloWorld(@RequestBody String to) {
-        var email =
-                new Email(new InternetAddress(to), List.of(), List.of(), "Formation subscribe", "You are subscribe", List.of());
-
-        mailer.accept(email);
+    public String subscribe(@RequestBody UserDto userDto) {
+        var event = SendEmailRequested.builder().to(userDto.getEmail()).build();
+        eventProducer.accept(List.of(event));
         return "You are subscribe";
     }
 }
