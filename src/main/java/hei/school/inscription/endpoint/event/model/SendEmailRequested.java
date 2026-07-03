8e509e34469e264cd7e129bd92e35b2a1033efaf
Randy <raoulrandy3@gmail.com>
2026-07-03 09:51:38 +0300

    update(service): add more verification for email

--- src/main/java/hei/school/inscription/endpoint/event/model/SendEmailRequested.java
+++ src/main/java/hei/school/inscription/endpoint/event/model/SendEmailRequested.java
@@ -1,5 +1,7 @@
 package hei.school.inscription.endpoint.event.model;
 import java.time.Duration;
+
+import hei.school.inscription.dto.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,7 +16,7 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = false)
 @ToString
 public class SendEmailRequested extends PojaEvent {
-    private String to;
+    private UserDto userDto;
 
     @Override
     public Duration maxConsumerDuration() {
--- src/main/java/hei/school/inscription/service/event/SendEmailRequestedService.java
+++ src/main/java/hei/school/inscription/service/event/SendEmailRequestedService.java
@@ -6,6 +6,10 @@ import hei.school.inscription.mail.Mailer;
 import jakarta.mail.internet.InternetAddress;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import jakarta.ws.rs.BadRequestException;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
@@ -18,7 +22,15 @@ public class SendEmailRequestedService implements Consumer<SendEmailRequested> {
     @SneakyThrows
     @Override
     public void accept(SendEmailRequested sendEmailRequested) {
-        InternetAddress recipientAddress = new InternetAddress(sendEmailRequested.getTo());
+        String email = sendEmailRequested.getUserDto().getEmail();
+        Pattern p = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
+        Matcher m = p.matcher(email);
+
+
+        if (!m.find()) {
+            throw new BadRequestException("email format is not respected");
+        }
+        InternetAddress recipientAddress = new InternetAddress();
         mailer.accept(new Email(recipientAddress, List.of(), List.of(), "Formation subscribe", "You are subscribe", List.of()));
     }
 }
